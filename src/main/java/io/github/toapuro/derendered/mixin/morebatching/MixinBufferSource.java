2ade9b1125b69378b028a06e7c5376e70e298b26
toapuro <164835903+toapuro@users.noreply.github.com>
2026-03-17 12:51:10 +0900

    feat: inject RenderType comparator

---
+++ src/main/java/io/github/toapuro/derendered/mixin/morebatching/MixinBufferSource.java
@@ -0,0 +1,30 @@
+package io.github.toapuro.derendered.mixin.morebatching;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
+import io.github.toapuro.derendered.api.render.morebatching.RenderTypeComparator;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+
+import java.util.Optional;
+
+@SuppressWarnings("OptionalUsedAsFieldOrParameterType")
+@Mixin(MultiBufferSource.BufferSource.class)
+public class MixinBufferSource {
+
+    @Shadow
+    protected Optional<RenderType> lastState;
+
+    @SuppressWarnings("OptionalIsPresent")
+    @ModifyExpressionValue(method = "getBuffer", at = @At(value = "INVOKE", target = "Ljava/util/Objects;equals(Ljava/lang/Object;Ljava/lang/Object;)Z"))
+    public boolean equals(boolean original,
+                          @Local(argsOnly = true) RenderType renderType) {
+        if(this.lastState.isPresent()) {
+            return original || RenderTypeComparator.contentEquals(this.lastState.get(), renderType);
+        }
+        return original;
+    }
+}
